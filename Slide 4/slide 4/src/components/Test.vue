71e5a016c540375c6d596ff832757fff3fef62fd
TS01469-Dat-VueJS <cunhi3806@gmail.com>
2026-01-15 13:35:02 +0700

    Hoàn thành

---
+++ Slide 4/slide 4/src/components/Test.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+    Number: {{ count }}
+    <button @click="increment">Add</button>
+    <button @click="reset">Reset</button>
+</div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            count: 0
+        };
+    },
+    methods: {
+        increment() {
+            this.count++;// Dùng this để truy cập biến trong data
+        },
+        reset() {
+            this.count = 0; //đặt lại count về 0
+        }
+    }
+};
+</script>
